35ddfd53337360c39c6c9b03d679388927026608
aldrichvalentino <aldrich.vh97@gmail.com>
2017-11-20 18:33:26 +0700

    user input

--- src/main.py
+++ src/main.py
@@ -1,1 +1,18 @@
-print "Tugas Besar Jarkom 2"
+# get user input for node topology
+numberOfNodes = eval(raw_input())
+arrayOfNodes = []
+for i in range(0, numberOfNodes):
+    arrayOfNodes.append(raw_input())
+
+# get user input for scenario
+numberOfScenarios = eval(raw_input())
+arrayOfScenarios = []
+for i in range(0, numberOfScenarios):
+    stringInput = raw_input()
+    result = stringInput.split(' ')
+    source = int(result[0])
+    destination = int(result[1])
+    arrayOfScenarios.append([source, destination])
+
+print arrayOfNodes
+print arrayOfScenarios
