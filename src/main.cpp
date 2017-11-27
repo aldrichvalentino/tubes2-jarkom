5cb2eef145aa30d483bd224189f82e1959ff7d5b
girvandip <girvandip@gmail.com>
2017-11-27 20:18:32 +0700

    Edited input form

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int numberOfNodes, numberOfEdges;
+    cin >> numberOfNodes >> numberOfEdges;
+    cout << numberOfNodes << endl;
+    cout << numberOfEdges << endl;
+    vector<>
+    for (int i = 0;i < numberOfEdges;i++) {
+
+    }
+    return 0;
+}
--- src/main.py
+++ src/main.py
@@ -4,78 +4,46 @@ userInput = nodesAndEdge.split(' ')
 numberOfNodes = eval(userInput[0])
 numberOfEdge = eval(userInput[1])
 
-# initiate array of nodes
-arrayOfNodes = []
-for i in range(0, numberOfNodes):
-    arrayOfNodes.append([])
-
-# fill the edge
-for i in range(0, numberOfEdge):
-    stringInput = raw_input()
-    result = stringInput.split(' ')
-    sourceEdge = eval(result[0]) - 1 
-    destinationEdge = eval(result[1]) - 1
-    neighbourNodes = arrayOfNodes[sourceEdge]
-    # insert the new neighbour
-    neighbourNodes.append(destinationEdge)
-    arrayOfNodes[sourceEdge] = neighbourNodes
-    # vice versa
-    neighbourNodes = arrayOfNodes[destinationEdge]
-    neighbourNodes.append(sourceEdge)
-    arrayOfNodes[destinationEdge] = neighbourNodes
-
-# get user input for scenario
-numberOfScenarios = eval(raw_input())
-arrayOfScenarios = []
-for i in range(0, numberOfScenarios):
-    stringInput = raw_input()
-    result = stringInput.split(' ')
-    source = int(result[0]) - 1  # begin with zero
-    destination = int(result[1]) - 1  # begin with zero
-    arrayOfScenarios.append([source, destination])
-
-# print arrayOfNodes
-# print arrayOfScenarios
-
-# initiate the routing table
 listOfRoutingTables = []
-for i in range(0, numberOfNodes):
-    # get the neighbour node list
-    neighbourNodes = arrayOfNodes[i]
-    
-    # make a routing table, initiate everything with -1
-    routingTable = []  # contains [distance, next hop]
-    for j in range(0, numberOfNodes):
-        if(j == i):  # routing to itself
-            routingTable.append([0, j])
+distanceAndnextHop = [-1, -1]
+for i in range(numberOfNodes):
+    routingTable = []
+    for j in range(numberOfNodes):
+        if(i == j):
+            routingTable.append([0,j])
         else:
-            routingTable.append([-1, -1])
-    
-    # fill the routing table with known neighbours
-    for j in range(0, len(neighbourNodes)):
-        routingTable[neighbourNodes[j]] = [1, neighbourNodes[j]]
-    
-    # insert into the list of routing table
+            routingTable.append(distanceAndnextHop)
     listOfRoutingTables.append(routingTable)
+    
+for i in range(numberOfEdge):
+    edge = raw_input()
+    edgeResult = edge.split(' ')
+    sourceEdge = eval(edgeResult[0]) - 1
+    destinationEdge = eval(edgeResult[1]) - 1
+    listOfRoutingTables[sourceEdge][destinationEdge] = [1, destinationEdge]
+    # vice versa
+    listOfRoutingTables[destinationEdge][sourceEdge] = [1, sourceEdge]
 
-# for i in range(0, len(listOfRoutingTables)):
-#     print listOfRoutingTables[i]
+# get the scenarios
+numberOfScenarios = eval(raw_input())
 
 # iterate through the scenarios
-for i in range(0, len(arrayOfScenarios)):
-    source = arrayOfScenarios[i][0]
-    destination = arrayOfScenarios[i][1]
-    
+for i in range(numberOfScenarios):
+    scenario = raw_input()
+    scenarioResult = scenario.split(' ')
+    source = eval(scenarioResult[0]) - 1
+    destination = eval(scenarioResult[1]) - 1
+
     # update the destination routing table with the source routing table
     sourceRoutingTable = listOfRoutingTables[source]
     destinationRoutingTable = listOfRoutingTables[destination]
     # print sourceRoutingTable
-    # print destinationRoutingTable
+    print destinationRoutingTable
 
     # Update the routing table of destination node
-    for j in range(0, len(destinationRoutingTable)):
+    for j in range(len(destinationRoutingTable)):
         if(destinationRoutingTable[j][0] == -1):  # information unknown
-            # print 'harus diupdate nih'
+            print 'harus diupdate nih'
             # check if the source routing table has the info about the unknown node
             if(sourceRoutingTable[j][0] != -1):
                 # info is known, then the next hop is assigned to the source node
@@ -87,8 +55,8 @@ for i in range(0, len(arrayOfScenarios)):
             # if the distance of the source routing table is shorter, then update
             destinationRoutingTable[j] = [1 + sourceRoutingTable[j][0], source]
     
-    # print 'udah diupdate'
-    # print destinationRoutingTable
+    print 'udah diupdate'
+    print destinationRoutingTable
     # dump the routing table back into the list
     listOfRoutingTables[destination] = destinationRoutingTable
 
